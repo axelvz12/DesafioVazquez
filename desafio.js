67776280922692d582168f237f96f0cb4ce2fe2c
Axel Vazquez <axelmaximiliano.vazquez@gmail.com>
2023-12-25 19:39:28 -0300

    Segundo desafio

--- desafio.js
+++ desafio.js
@@ -1,91 +1,87 @@
 class ProductManager {
-    constructor() {
-      this.products = [];
-    }
-  
-    getProducts() {
-      return this.products;
-    }
-  
-    addProduct(product) {
-      if (this.isCodeUnique(product.code)) {
-        product.id = this.generateUniqueId();
-        this.products.push(product);
-      } else {
-        throw new Error("El código del producto ya existe");
-      }
-    }
-  
-    getProductById(id) {
-      const product = this.products.find((p) => p.id === id);
-      if (product) {
-        return product;
-      } else {
-        throw new Error("Producto no encontrado");
-      }
-    }
-  
-    isCodeUnique(code) {
-      return !this.products.some((p) => p.code === code);
-    }
-  
-    generateUniqueId() {
-      return '_' + Math.random().toString(36).substr(2, 9);
-    }
+  constructor() {
+    this.products = [];
   }
-  
-  // Crear instancia de ProductManager
-  const productManager = new ProductManager();
-  
-  // Obtener productos (debería devolver [])
-  console.log(productManager.getProducts());
-  
-  // Agregar un producto
-  try {
-    productManager.addProduct({
-      title: "Producto prueba",
-      description: "Este es un producto de prueba",
-      price: 200,
-      thumbnail: "Sin imagen",
-      code: "abc123",
-      stock: 25,
-    });
-    console.log("Producto agregado satisfactoriamente");
-  } catch (error) {
-    console.error(error.message);
+
+  generateId() {
+    // Función para generar un id único (puede ser más compleja en la realidad)
+    return Date.now().toString();
   }
-  
-  // Obtener productos (debería devolver el producto agregado)
-  console.log(productManager.getProducts());
-  
-  // Intentar agregar un producto con el mismo código (debería arrojar un error)
-  try {
-    productManager.addProduct({
-      title: "Producto repetido",
-      description: "Este es otro producto de prueba",
-      price: 150,
-      thumbnail: "Otra imagen",
-      code: "abc123",
-      stock: 30,
-    });
-    console.log("Producto agregado satisfactoriamente");
-  } catch (error) {
-    console.error(error.message);
+
+  getProducts() {
+    return this.products;
   }
-  
-  // Obtener producto por ID (debería arrojar un error porque no existe)
-  try {
-    const product = productManager.getProductById("nonexistent_id");
-    console.log(product);
-  } catch (error) {
-    console.error(error.message);
+
+  addProduct(product) {
+    const id = this.generateId();
+    const newProduct = { id, ...product };
+    this.products.push(newProduct);
+    return newProduct;
   }
-  
-  // Obtener producto por ID (debería devolver el producto agregado anteriormente)
-  try {
-    const product = productManager.getProductById(productManager.getProducts()[0].id);
-    console.log(product);
-  } catch (error) {
-    console.error(error.message);
+
+  getProductById(id) {
+    const product = this.products.find((p) => p.id === id);
+    if (!product) {
+      throw new Error('Producto no encontrado');
+    }
+    return product;
   }
-  
+
+  updateProduct(id, updatedFields) {
+    const index = this.products.findIndex((p) => p.id === id);
+    if (index === -1) {
+      throw new Error('Producto no encontrado');
+    }
+
+    this.products[index] = { ...this.products[index], ...updatedFields };
+    return this.products[index];
+  }
+
+  deleteProduct(id) {
+    const index = this.products.findIndex((p) => p.id === id);
+    if (index === -1) {
+      throw new Error('Producto no encontrado');
+    }
+
+    const deletedProduct = this.products.splice(index, 1)[0];
+    return deletedProduct;
+  }
+}
+
+// Uso de la clase
+const productManager = new ProductManager();
+
+// Obtener productos (debería ser un arreglo vacío)
+console.log(productManager.getProducts());
+
+// Agregar un producto
+const newProduct = {
+  title: 'producto prueba',
+  description: 'Este es un producto prueba',
+  price: 200,
+  thumbnail: 'Sin imagen',
+  code: 'abc123',
+  stock: 25,
+};
+
+const addedProduct = productManager.addProduct(newProduct);
+console.log('Producto agregado:', addedProduct);
+
+// Obtener productos nuevamente (debería contener el producto recién agregado)
+console.log(productManager.getProducts());
+
+// Obtener producto por id
+const productId = addedProduct.id;
+console.log('Producto por ID:', productManager.getProductById(productId));
+
+// Actualizar producto
+const updatedFields = { price: 250, stock: 30 };
+const updatedProduct = productManager.updateProduct(productId, updatedFields);
+console.log('Producto actualizado:', updatedProduct);
+
+// Eliminar producto
+const deletedProduct = productManager.deleteProduct(productId);
+console.log('Producto eliminado:', deletedProduct);
+
+// Obtener productos después de la eliminación (debería ser un arreglo vacío nuevamente)
+console.log(productManager.getProducts());
